1b1d600ae54a056514c885edd998e8bf36145e4c
Chris Lowder <clowder@gmail.com>
2022-10-11 08:48:39 +0100

    Check for successful startup and wait on shutdown
    Hailmary to remove connection errors around the daily restart. The aim
    is for the dyno to wait for Cloud Proxy to start successfully and for
    Cloud Proxy to be the last thing to exit on shutdown.

--- profile/gcloud.sh
+++ profile/gcloud.sh
@@ -6,7 +6,12 @@ export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.gcloud/creds.json"
 export PATH="$HOME/.gcloud/bin:$PATH"
 
 if [[ -t 0 ]]; then
-  (setsid cloud_sql_proxy "$GCLOUD_SQL_INSTANCE" > "$HOME/.gcloud/var/log/cloud_sql_proxy.log" 2>&1) &
+  (setsid cloud_sql_proxy --health-check --max-sigterm-delay "10s" "$GCLOUD_SQL_INSTANCE" > "$HOME/.gcloud/var/log/cloud_sql_proxy.log" 2>&1) &
 else
-  (setsid cloud_sql_proxy "$GCLOUD_SQL_INSTANCE") &
+  (setsid cloud_sql_proxy --health-check --max-sigterm-delay "10s" "$GCLOUD_SQL_INSTANCE") &
+fi
+
+if ! curl -sf --retry 5 --retry-all-errors --retry-delay 1 http://localhost:9090/startup > /dev/null; then
+  echo "Google Cloud SQL Proxy failed to start."
+  exit 1
 fi
